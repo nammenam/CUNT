499d939d3073a4ead70fea6d5caa871a2f1d870d
Brage <bragewiseth@icloud.com>
2026-03-13 21:27:49 +0100

    init

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,10 @@ const config = {
       fallback: '404.html',
       precompress: false,
       strict: true
-    })
+    }),
+    paths: {
+      base: process.argv.includes('dev') ? '' : '/CUNT'
+    }
   }
 };
 
